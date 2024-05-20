6441f42b587ba661d502c5ba208b12204269493c
LalithyaMaddumage <sandeepanilalithya155@gmail.com>
2024-05-21 04:35:18 +0530

    chat-bot-code-SMSSERVICE-UPDATE

--- app/services/chat/smsServices/newSmsService.js
+++ app/services/chat/smsServices/newSmsService.js
@@ -1,15 +1,15 @@
 const https = require('follow-redirects').https;
 
 const options = {
-    method: 'POST',
-    hostname: '2v4y9m.api.infobip.com',
-    path: '/sms/2/text/advanced',
-    headers: {
-        'Authorization': 'App 284c3501c72d6a8671d2c1a1b5eec224-6c4535a9-9311-4d88-b8e9-f4235277626d',
+    'method': 'POST',
+    'hostname': 'k2epj1.api.infobip.com',
+    'path': '/sms/2/text/advanced',
+    'headers': {
+        'Authorization': 'App a187fa12022eb5e3205bffe98918b7ff-e5a45965-beb5-4a6f-b3b5-5b2ab854ec77',
         'Content-Type': 'application/json',
         'Accept': 'application/json'
     },
-    maxRedirects: 20
+    'maxRedirects': 20
 };
 
 const sendSms = (body) => {
